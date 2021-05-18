4a0a56e19f9fce28e45698cf88c5237eb0b102b2
hpgross <hpgross@gmail.com>
2021-05-18 13:58:21 -0400

    Fixed a comparison operator mistake in the script

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -9,6 +9,6 @@ data_loc = os.getcwd()+"/supplier-data/images"
 file_list = os.listdir(data_loc)
 
 for file in file_list:
-    if file[-5:] = ".jpeg":
+    if file[-5:] == ".jpeg":
         with open(data_loc+'/'+file, 'rb') as opened:
             r = requests.post(url, files={'file': opened})
