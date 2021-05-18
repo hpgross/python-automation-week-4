1ec60e73e0c0c01cb97751f1574e60150c2c25b5
hpgross <hpgross@gmail.com>
2021-05-18 13:53:24 -0400

    Wrote the supplier_image_upload.py script

---
+++ supplier_image_upload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+import os
+# This example shows how a file can be uploaded using
+# The Python Requests module
+
+url = "http://localhost/upload/"
+data_loc = os.getcwd()+"/supplier-data/images"
+file_list = os.listdir(data_loc)
+
+for file in file_list:
+    if file[-5:] = ".jpeg":
+        with open(data_loc+'/'+file, 'rb') as opened:
+            r = requests.post(url, files={'file': opened})
