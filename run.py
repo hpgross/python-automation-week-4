ecac01641c91b29c2ba2e527678728f4532f5b7b
hpgross <hpgross@gmail.com>
2021-05-18 14:29:54 -0400

    Crated the script to upload descriptions

---
+++ run.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+
+import os
+import requests
+
+ip_address = ""
+server_location = "http://" + ip_address + "/fruits/"
+file_list = os.listdir("/supplier-data/descriptions")
+current_request = {}
+
+for file in file_list:
+    if file[-4:] == ".txt":
+        #set up the dictionary for the request
+        open_file = open("/supplier-data/descriptions/"+file,"r")
+        line_list = open_file.read().splitlines()
+        current_request["name"] = line_list[0]
+        current_request["weight"] = Int(line_list[1][0:-4])
+        current_request["description"] = line_list[2]
+        current_request["image_name"] = file[-4:0]+ ".jpeg"
+        open_file.close()
+
+        new_post = requests.post(server_location,json=current_request)
+        new_post.raise_for_status()
+        print(new_post.status_code)
